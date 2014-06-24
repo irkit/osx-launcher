71fc8e2335b8ddc31f43d2aac595a9dcc245154a
Masakazu OHTSUKA <o.masakazu@gmail.com>
2014-06-24 23:18:42 +0900

    Add com.apple.ResourceFork and com.apple.FinderInfo as binary data in project, and add those attributes to IR signal JSON representation files.

--- IRLauncher/ILFileStore.m
+++ IRLauncher/ILFileStore.m
@@ -13,6 +13,7 @@
 
 #import "ILLog.h"
 #import "ILConst.h"
+#import <sys/xattr.h>
 
 static NSString * const kILConfigDirectory     = @".irkit.d/";
 static NSString * const kILSignalsSubDirectory = @"signals/";
@@ -135,7 +136,33 @@ static NSString * const kILSignalsSubDirectory = @"signals/";
         ILLOG( @"failed to write to: %@", file );
         return NO;
     }
+
+    [self setSignalExtendedAttributesToFile: file];
+
     return YES;
+}
+
+#pragma mark - Private
+
++ (int)setSignalExtendedAttributesToFile:(NSString*)file {
+    NSString *finderInfoFile   = [[NSBundle mainBundle] pathForResource:@"FinderInfo"   ofType:@"dat"];
+    NSString *resourceForkFile = [[NSBundle mainBundle] pathForResource:@"ResourceFork" ofType:@"dat"];
+    NSData *finderInfo   = [NSData dataWithContentsOfFile:finderInfoFile];
+    NSData *resourceFork = [NSData dataWithContentsOfFile:resourceForkFile];
+
+    int result;
+    result = setxattr([file fileSystemRepresentation], XATTR_FINDERINFO_NAME, finderInfo.bytes, finderInfo.length, 0, 0);
+    if (result != 0) {
+        perror("setxattr finderinfo");
+        return result;
+    }
+    result = setxattr([file fileSystemRepresentation], XATTR_RESOURCEFORK_NAME, resourceFork.bytes, resourceFork.length, 0, 0);
+    if (result != 0) {
+        perror("setxattr resourcefork");
+        return result;
+    }
+
+    return result;
 }
 
 @end
