c802cbdcd01910930db73252799f3f3203cbb6e9
Masakazu OHTSUKA <o.masakazu@gmail.com>
2014-06-26 16:32:03 +0900

    No longer needed, we use extended attributes to show custom icon on JSON files and use custom application to launch IRLauncher.app

--- IRSender/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-osacompile -o IRSender.scpt IRSender.applescript
-Rez -append resources/IRSender.rsrc -o IRSender.scpt
-SetFile -a C IRSender.scpt
-cp IRSender.scpt "$HOME/Library/Application Support/Quicksilver/Actions/IRSender.scpt"
